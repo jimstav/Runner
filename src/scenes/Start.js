a96f98492747d604d09812604c5112b09c6d01e9
Dimitris Stavroulakis <jimstav@gmail.com>
2025-07-07 22:02:48 +0300

    Init project

---
+++ src/scenes/Start.js
@@ -0,0 +1,19 @@
+export class Start extends Phaser.Scene {
+
+    constructor() {
+        super('Start');
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+        
+    }
+
+    update() {
+        
+    }
+    
+}
